429319250095f5a0123fe7e33d6e581f4bb1ba84
Andrea Sciamanna <andrea.s@onthegosystems.com>
2021-04-01 18:17:33 +0200

    When rendering the LS as a menu item, we use a fairly different HTML
    - Added extra logic for when the HTML input is for a single language elements (as opposed to the full LS HTML as in other cases)
    - Wrapping the HTML with a dummy container, allows to extract the final HTML even when there are adjacent elements

--- src/ForceAltAttributeInFlags.php
+++ src/ForceAltAttributeInFlags.php
@@ -18,11 +18,14 @@ class ForceAltAttributeInFlags {
 		$display_flags  = $slot->get( 'display_flags' );
 		$display_native = $slot->get( 'display_names_in_native_lang' );
 		$display_name   = $slot->get( 'display_names_in_current_lang' );
+		$group          = $slot->get( 'slot_group' );
 
 		if ( $display_flags && ( $display_native || $display_name ) ) {
 			$dom = new \DOMDocument();
-			$dom->loadHTML( $html, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD );
+			$dom->loadHTML( '<div id="wpml-fix-alt">' . $html . '</div>', LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD );
 			$xpath = new \DOMXPath( $dom );
+
+			$container = $dom->getElementById( 'wpml-fix-alt' );
 
 			$nodes = $xpath->query( '//img[ @class="wpml-ls-flag" ]' );
 
@@ -30,20 +33,56 @@ class ForceAltAttributeInFlags {
 			foreach ( $nodes as $node ) {
 				$altTexts = [];
 
-				if ( $display_native ) {
-					$altTexts[] = $this->getLanguageName( $xpath, $node, 'wpml-ls-native' );
-				}
-				if ( $display_name ) {
-					$altTexts[] = $this->getLanguageName( $xpath, $node, 'wpml-ls-display' );
+				if ( $group === 'menus' ) {
+					if ( $display_native ) {
+						$altTexts[] = $this->getLanguageName( $xpath, 'wpml-ls-native' );
+					}
+					if ( $display_name ) {
+						$altTexts[] = $this->getLanguageName( $xpath, 'wpml-ls-display' );
+					}
+				} else {
+
+					if ( $display_native ) {
+						$altTexts[] = $this->getLanguageNameFromSiblings( $xpath, $node, 'wpml-ls-native' );
+					}
+					if ( $display_name ) {
+						$altTexts[] = $this->getLanguageNameFromSiblings( $xpath, $node, 'wpml-ls-display' );
+					}
 				}
 
-				$node->setAttribute( 'alt', join( '', array_filter( $altTexts ) ) );
+				$alt_value = trim( join( '', array_filter( $altTexts ) ) );
+				if ( $alt_value ) {
+					$node->setAttribute( 'alt', $alt_value );
+				}
 			}
 
-			return $dom->saveHTML();
+			$htmlElements = [];
+
+			foreach ( $container->childNodes as $childNode ) {
+				$htmlElements[] = $dom->saveHTML( $childNode );
+			}
+
+			$save_HTML = join( PHP_EOL, $htmlElements );
+
+			return $save_HTML;
 		}
 
 		return $html;
+	}
+
+	/**
+	 * @param \DOMXPath $xpath
+	 * @param string    $class
+	 *
+	 * @return string
+	 */
+	private function getLanguageName( $xpath, $class ) {
+		$element = $xpath->query( 'span[ @class="' . $class . '" ]' );
+		if ( $element->count() > 0 && $element->item( 0 )->textContent ) {
+			return $element->item( 0 )->textContent;
+		}
+
+		return '';
 	}
 
 	/**
@@ -53,15 +92,15 @@ class ForceAltAttributeInFlags {
 	 *
 	 * @return string
 	 */
-	private function getLanguageName( $xpath, $node, $class ) {
-		$element = 'span[ @class="' . $class . '"]';
+	private function getLanguageNameFromSiblings( $xpath, $node, $class ) {
+		$element = 'span[ @class="' . $class . '" ]';
 
-		$followingSiblings = $xpath->query( 'following-sibling::' . $element, $node );
+		$followingSiblings = $xpath->query( ( $node ? 'following-sibling::' : '' ) . $element, $node );
 		if ( $followingSiblings->count() > 0 && $followingSiblings->item( 0 )->textContent ) {
 			return $followingSiblings->item( 0 )->textContent;
 		}
 
-		$precedingSiblings = $xpath->query( 'following-sibling::' . $element, $node );
+		$precedingSiblings = $xpath->query( ( $node ? 'preceding-sibling::' : '' ) . $element, $node );
 		if ( $precedingSiblings->count() > 0 && $precedingSiblings->item( 0 )->textContent ) {
 			return $precedingSiblings->item( 0 )->textContent;
 		}
